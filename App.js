575dc2c8145bd617383d9e979af1e96d271c31be
C1324655 Ruan Pissinati de Lyra Revoredo <c1324655@interno.bb.com.br>
2022-03-18 18:17:28 -0300

    Express - Essencial add comentarios sobre uso do bodyparse.json e .urlencoded

--- App.js
+++ App.js
@@ -1,4 +1,10 @@
-const express = require('exprees');
+const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json()); // Entenda quando enviar parametros em json
+app.use(bodyParser.urlencoded({})) // Entenda quando enviar parametro via url e decodar esses parametros
+
+app.listen(3000);
+
