0ec6c5c4c0347c8e8c3a92b41dd416f7a01843dd
C1324655 Ruan Pissinati de Lyra Revoredo <c1324655@interno.bb.com.br>
2022-03-18 18:21:48 -0300

    App - rota de teste

--- App.js
+++ App.js
@@ -4,7 +4,13 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.json()); // Entenda quando enviar parametros em json
-app.use(bodyParser.urlencoded({})) // Entenda quando enviar parametro via url e decodar esses parametros
+app.use(bodyParser.urlencoded({extended: false})) // Entenda quando enviar parametro via url e decodar esses parametros
+
+app.get('/', (req, res)=>{
+    // req serve como parametro para receber um objetos de uma requisiçao.
+    // rep é o objeto usado para enviar resposta referentes a requisição.
+    res.send('OK')
+
+})
 
 app.listen(3000);
-
